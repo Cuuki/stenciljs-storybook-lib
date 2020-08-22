f449a83ee14b4025cdadeb309494877fa17ee94f
Patrick Söllner <mail@patrick-soellner.de>
2020-08-22 18:47:41 +0200

    test react lib output build

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,15 +1,20 @@
 import { Config } from '@stencil/core';
+import { reactOutputTarget } from '@stencil/react-output-target';
 
 export const config: Config = {
   namespace: 'stenciljs-storybook-lib',
   taskQueue: 'async',
   outputTargets: [
+    reactOutputTarget({
+      componentCorePackage: '@cuuki/stenciljs-storybook-lib',
+      proxiesFile: '../@cuuki\/stenciljs-storybook-lib-react/src/components.ts',
+    }),
     {
       type: 'dist',
       esmLoaderPath: '../loader',
     },
     {
-      type: 'dist-custom-elements-bundle'
+      type: 'dist-custom-elements-bundle',
     },
     {
       type: 'docs-readme',
